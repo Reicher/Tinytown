f3b4b3e719ab9ccf0f8f43b6013c1faf4e0f08de
Reicher <robin.o.reicher@gmail.com>
2017-06-11 13:41:40 +0200

    added sky and rotation to it.
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/game.js
+++ src/game.js
@@ -15,9 +15,13 @@ export default class extends Phaser.State {
 	this.planet.pivot.x = 250;
 	this.planet.pivot.y = 500;
 
+	this.planet.sky = this.planet.create(250, 500, 'sky')
+	this.planet.sky.anchor.setTo(0.5);
+
 	this.planet.create(0, 250, 'planet')
 	this.planet.create(225, 155, 'tree1')
-	this.planet.rotation = 60 - game.rnd.frac() * 120
+
+	// this.planet.rotation = game.rnd.frac()
 
 	this.hero = this.add.sprite(225,155, 'hero')
 	this.hero.animations.add('west', [12, 13, 14, 15, 16, 17, 18, 19], 15, true);
@@ -27,15 +31,32 @@ export default class extends Phaser.State {
     }
 
     update () {
+	var speed = 0.5
 	if (this.cursors.left.isDown){
-	    this.planet.rotation += 0.01
+	    this.planet.angle += speed
 	    this.hero.animations.play('west')
 	}
 	else if (this.cursors.right.isDown){
-	    this.planet.rotation -= 0.01
+	    this.planet.angle -= speed
 	    this.hero.animations.play('east')
 	}
 	else
 	    this.hero.frame = 2
+
+	this.planet.angle %= 359// Cannot use 360 for whatever reason.
+	this.planet.sky.angle -= 0.005
+
+        console.log("Planet angle: " + parseInt(this.planet.angle))
+	this.setLum();
+    }
+
+    setLum() {
+	var light = 0
+	if(Math.abs(this.planet.angle) < 180)
+	    light = Math.abs(this.planet.angle) / 180
+	else
+	    light = 360 / Math.abs(this.planet.angle)
+
+	this.planet.sky.alpha = light;
     }
 }
