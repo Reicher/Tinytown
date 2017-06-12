d150bc64da6de05443daa4ee1aade056bb63c5e8
Reicher <robin.o.reicher@gmail.com>
2017-06-12 23:01:39 +0200

    scale up the knug
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/game.js
+++ src/game.js
@@ -19,6 +19,7 @@ export default class extends Phaser.State {
 	this.hero.animations.add('west', [12, 13, 14, 15, 16, 17, 18, 19], 15, true);
 	this.hero.animations.add('east', [28, 29, 30, 31, 32, 33, 34, 35], 15, true);
 	this.hero.anchor.setTo(0.5, 1)
+	this.hero.scale.setTo(1.5)
 
 	this.tint = this.add.graphics(0, 0)
 	this.tint.beginFill(0xffffff, 0.2);
