83285e25e8bed294cab2558ed2729c7d4f7ca873
Reicher <robin.o.reicher@gmail.com>
2017-06-15 22:59:16 +0200

    Added ambient light
    Signed-off-by: Reicher <robin.o.reicher@gmail.com>

--- src/game.js
+++ src/game.js
@@ -1,10 +1,8 @@
-/* globals __DEV__ */
 import Phaser from 'phaser'
 
-import Space from './space.js'
-import Planet from './planet.js'
 import Hero from './hero.js'
 import Level from './level.js'
+import Lights from './lights'
 
 export default class extends Phaser.State {
     init () {}
@@ -13,22 +11,14 @@ export default class extends Phaser.State {
 
     create () {
 
-	this.level = new Level(this.game)
-
+	this.level = new Level(game)
 	this.hero = new Hero(game, 250, 255)
-
-	// this.tint = this.add.graphics(0, 0)
-	// this.tint.beginFill(0xffffff, 0.2);
-	// this.tint.drawRect(0, 0, 500, 500);
-	// this.tint.endFill();
+	this.lights = new Lights(game);
     }
 
     update () {
 	this.level.angle += this.hero.movement
-
-	// var time = Math.abs(Math.sin((this.planet.rotation) *0.5))
-
-	// var c = Phaser.Color.interpolateColor(0x003366, 0xFDB813, 100, 100 * time, 1)
-	// this.tint.tint = c
+	this.lights.hero_rot = this.level.rotation;
+	this.lights.update(this.level.rotation);
     }
 }
---
+++ src/lights.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser'
+
+export default class Lights extends Phaser.Group {
+    constructor(game) {
+	super(game)
+	this.tint = game.add.graphics(0, 0)
+	this.tint.beginFill(0xffffff, 0.15);
+	this.tint.drawRect(0, 0, 500, 500);
+	this.tint.endFill();
+	this.add(this.tint)
+
+	this.hero_rot = 0;
+    }
+
+    update() // in radians
+    {
+	var light = Math.abs(Math.sin(this.hero_rot *0.5))
+	var c = Phaser.Color.interpolateColor(0x003366,
+					      0xFDB813,
+					      100,
+					      100 * light,
+					      1)
+	this.tint.tint = c
+    }
+}
